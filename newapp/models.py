437cc9bcb39206b740e408b62e9680ad1e1e0022
Dagim-Zeynu <dagim35z@gmail.com>
2024-06-15 19:44:59 +0300

    first commit

---
+++ newapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    role = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10)
+    email_address = models.EmailField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.first_name
